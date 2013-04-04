eaa2c79e43238a067bd4585930c17bee9f9dc4c8
Charlie Savage <cfis@zerista.com>
2013-04-03 22:32:24 -0600

    Make osx static binary.

---
+++ MRCEnumerableOSX/MRCEnumerableOSX.h
@@ -0,0 +1,13 @@
+//
+//  MRCEnumerableOSX.h
+//  MRCEnumerableOSX
+//
+//  Created by Charlie Savage on 4/1/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MRCEnumerableOSX : NSObject
+
+@end
---
+++ MRCEnumerableOSX/MRCEnumerableOSX.m
@@ -0,0 +1,13 @@
+//
+//  MRCEnumerableOSX.m
+//  MRCEnumerableOSX
+//
+//  Created by Charlie Savage on 4/1/13.
+//
+//
+
+#import "MRCEnumerableOSX.h"
+
+@implementation MRCEnumerableOSX
+
+@end
